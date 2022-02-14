1a9934d6e633fd519f1f01b348d091b7f2b3fad1
christopherzito <christopherzito11@gmail.com>
2022-02-14 13:42:23 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const app = new Vue({
+        el: '#app',
+    });
+    
+});
